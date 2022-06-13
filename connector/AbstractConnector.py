07bca4cd61704c784a1bebeb79b1392e03c06468
Romain Neil <contact@romain-neil.fr>
2022-06-13 18:52:59 +0200

    feat(connector/AbstractConnector.py): add pydoc

--- connector/AbstractConnector.py
+++ connector/AbstractConnector.py
@@ -7,18 +7,36 @@ class AbstractConnector(ABC):
     def auth(self, username, password):
         """
         Authenticate to the service
-        :param username:
-        :param password:
-        :return:
+
+        Parameters
+        ----------
+        username : str
+            The service username
+        password: str
+            The service password
         """
         pass
 
     @abstractmethod
     def set_instance(self, url):
-        """Set the instance url"""
+        """
+        Set the instance url
+
+        Parameters
+        ----------
+        url : str
+            The instance url
+        """
         pass
 
     @abstractmethod
     def update_dns(self, domain, ip):
-        """Update the dns ip to the service"""
+        """
+        Update the dns ip to the service
+
+        Parameters
+        ----------
+        domain : str
+        ip : str
+        """
         pass
