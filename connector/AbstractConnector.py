b44e2052036f04d899df95a03e528339d6c02cbe
Romain Neil <contact@romain-neil.fr>
2022-08-03 14:46:39 +0200

    feat: add function to have optionals parameters

--- connector/AbstractConnector.py
+++ connector/AbstractConnector.py
@@ -2,6 +2,24 @@ from abc import abstractmethod, ABC
 
 
 class AbstractConnector(ABC):
+    """
+    Represent an abstract connector, to be inherited by dns software' connectors
+    """
+
+    @abstractmethod
+    def get_optionals_parameters(self) -> list[str]:
+        """
+        Define a list of optionals parameters required by the connector
+        The function return a list of parameter name, set in the connnector by calling set_optional_param(param_name, param_value)
+        """
+        pass
+
+    @abstractmethod
+    def set_optional_parameter(self, param_name: str, param_value: str):
+        """
+        @see get_optionals_parameters
+        """
+        pass
 
     @abstractmethod
     def auth(self, username: str, password: str):
