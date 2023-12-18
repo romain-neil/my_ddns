f0978cb24dda2a8e42725971f54b32d92e4c09c5
Romain Neil <contact@romain-neil.fr>
2023-12-18 21:26:37 +0100

    wip: cloudflare connector

--- connector/CloudflareConnector.py
+++ connector/CloudflareConnector.py
@@ -1,4 +1,10 @@
+import json
+
+import requests
+
 from connector.AbstractConnector import AbstractConnector
+from util.dns.Record import Record
+from util.text import success, error
 
 
 class CloudflareConnector(AbstractConnector):
@@ -8,17 +14,53 @@ class CloudflareConnector(AbstractConnector):
     """
 
     def __init__(self):
-        self.instance_url = None
-        self.password = None
-        self.username = None
+        self.instance_url = "api.cloudflare.com/client"
+        self.api_version = "v4"
 
         self.params = {}
 
     def get_optionals_parameters(self) -> list[str]:
-        pass
+        return [
+            'has_api_key',
+            'domain',
+            'ttl',
+            'proxied'
+        ]
 
     def set_optional_parameter(self, param_name: str, param_value: str):
-        pass
+        self.params[param_name] = param_value
+
+    def build_headers(self):
+        auth_header = 'X-Auth-Email'
+
+        if self.params.get('has_api_key', False):
+            auth_header = 'X-Api-Key'
+
+        return {
+            # auth_header: self.params.
+        }
+
+    def build_params(self, domain: str, record: Record) -> str:
+        return json.dumps({
+            "content": record.get_ip(),
+            "name": domain,
+            "type": record.get_record_type().name,
+            "ttl": self.params.get('ttl', 3600),
+            "proxied": self.params.get('proxied', 'false')
+        })
 
     def update_dns(self, domain: str, ip: str):
-        pass
+        record = Record(str(ip))
+
+        r = requests.put(
+            url=f"https://{self.instance_url}/{self.api_version}/zones/zone_identifier/dns_records/identifier",
+            headers=self.build_headers()
+        )
+
+        if 200 <= r.status_code < 300:
+            success(f"[SUCCESS]: Update domain to {record.content}")
+        else:
+            resp = json.loads(r.content)
+
+            error(f"[ERROR]: Cannot update domain")
+            error(f"{resp.error.message}")
